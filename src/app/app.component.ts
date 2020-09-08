663323624c1e8071089c448ef6b80f5fd83ba338
Karelin Alexander <a.karelin@i-teco.ru>
2020-09-08 21:47:30 +0300

    создал таблицу уроков

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {v4 as uuidV4} from 'uuid';
+import {MatTable} from '@angular/material/table';
+import moment from 'moment';
+
+export interface Lesson {
+  id: string;
+  date?: Date;
+  theme?: string;
+  homework?: string;
+  notes?: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'less4-task1';
+  lessons: Lesson[]  = [
+    {id: uuidV4(),  date: new Date(), theme: 'Как ангуляриять', homework: 'сделать всё', notes: 'Было здорово'},
+    {id: uuidV4(),  date: new Date() , theme: 'Как ангуляриятьdd', homework: 'сделать ddd всё', notes: 'Было dcc здорово'},
+  ];
+  displayedColumns = [
+    'number', 'date', 'theme', 'homework', 'notes', 'edit'
+  ];
+  formVisible = false;
+  editingLesson = {} as any;
+  @ViewChild('lessonTable') lessonTable: MatTable<Lesson>;
+  ngOnInit(): void {
+    const recoveredLessons = JSON.parse(localStorage.getItem('lessonList'));
+    this.lessons = recoveredLessons.map((item) => {
+      return {...item, date: new Date(item.date)};
+    });
+  }
+
+  editRow = (id) => {
+    if (id) {
+      const lessonToEdit = this.lessons.find((item) => item.id === id);
+      this.editingLesson = {...lessonToEdit, date: moment(lessonToEdit.date).format('YYYY-MM-DD')};
+    } else {
+      this.editingLesson = {id: uuidV4()};
+    }
+    this.formVisible = true;
+  }
+
+  afterEdit = () => {
+    const index = this.lessons.findIndex((item) => this.editingLesson.id === item.id);
+    const lessonToSave = {...this.editingLesson, date: new Date(this.editingLesson.date)};
+    if (index < 0) {
+      this.lessons.push(lessonToSave);
+    } else {
+      this.lessons[index] = lessonToSave;
+    }
+    this.formVisible = false;
+    localStorage.setItem('lessonList', JSON.stringify(this.lessons.sort((a, b) => (a.date.getTime() - b.date.getTime()))));
+    this.lessonTable.renderRows();
+}
+  cancelEdit = () => {
+    this.formVisible = false;
+    this.lessonTable.renderRows();
+  }
+
+  deleteLesson = (id) => {
+    this.formVisible = false;
+    this.lessons = this.lessons.filter((item) => item.id !== id);
+    localStorage.setItem('lessonList', JSON.stringify(this.lessons.sort((a, b) => (a.date.getTime() - b.date.getTime()))));
+    this.lessonTable.renderRows();
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
